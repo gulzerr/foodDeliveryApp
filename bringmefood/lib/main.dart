8b7b4425a02162210729c07d06b68c54682fcaa8
Bulbul Gulzer <gulzer.deb@gtmail.com>
2020-03-17 17:24:21 +0600

    vule gesi ki update korsilam

--- bringmefood/lib/main.dart
+++ bringmefood/lib/main.dart
@@ -1,160 +1,178 @@
 import 'Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
-import 'package:passwordfield/passwordfield.dart';
 
-void main() => runApp(
-  MaterialApp(
+void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
-    title:"Bring me Food"
-  )
-);
+    title: "Bring me Food"));
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-      	child: Container(
-					width: MediaQuery.of(context).size.width,
-					height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/unnamed.png'),
-                  fit: BoxFit.fill
-              )
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/unnamed.png'),
+                    fit: BoxFit.fill)),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 300,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child: FadeAnimation(
+                            3,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/light-1.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: FadeAnimation(
+                            3,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/light-2.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: FadeAnimation(
+                            3,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/clock.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        left: 100,
+                        top: 140,
+                        width: 200,
+                        height: 150,
+                        child: FadeAnimation(
+                            3,
+                            Container(
+                              margin: EdgeInsets.only(top: 50),
+                              child: Center(
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeAnimation(
+                          3,
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(143, 148, 251, .2),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              color: Colors.grey[100]))),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Email or Phone number",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey[400])),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey[400])),
+                                    obscureText: true,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      FadeAnimation(
+                          3,
+                          Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(colors: [
+                                  Colors.grey,
+                                  Colors.blueGrey,
+                                  Colors.grey
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                        height: 110,
+                      ),
+                      FadeAnimation(
+                          3,
+                          Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.white70),
+                          )),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-	        child: Column(
-	          children: <Widget>[
-	            Container(
-	              height: 300,
-
-	              child: Stack(
-	                children: <Widget>[
-	                  Positioned(
-	                    left: 30,
-	                    width: 80,
-	                    height: 200,
-	                    child: FadeAnimation(1, Container(
-	                      decoration: BoxDecoration(
-	                        image: DecorationImage(
-	                          image: AssetImage('assets/images/light-1.png')
-	                        )
-	                      ),
-	                    )),
-	                  ),
-	                  Positioned(
-	                    left: 140,
-	                    width: 80,
-	                    height: 150,
-	                    child: FadeAnimation(1.3, Container(
-	                      decoration: BoxDecoration(
-	                        image: DecorationImage(
-	                          image: AssetImage('assets/images/light-2.png')
-	                        )
-	                      ),
-	                    )),
-	                  ),
-	                  Positioned(
-	                    right: 40,
-	                    top: 40,
-	                    width: 80,
-	                    height: 150,
-	                    child: FadeAnimation(1.5, Container(
-	                      decoration: BoxDecoration(
-	                        image: DecorationImage(
-	                          image: AssetImage('assets/images/clock.png')
-	                        )
-	                      ),
-	                    )),
-	                  ),
-	                  Positioned(
-											left: 100,
-											top: 140,
-											width: 200,
-											height: 150,
-	                    child: FadeAnimation(1.6, Container(
-	                      margin: EdgeInsets.only(top: 50),
-	                      child: Center(
-	                        child: Text("Login", style: TextStyle(color: Colors.white,
-															fontSize: 40, fontWeight: FontWeight.bold),),
-	                      ),
-	                    )),
-	                  )
-	                ],
-	              ),
-	            ),
-	            Padding(
-	              padding: EdgeInsets.all(30.0),
-	              child: Column(
-	                children: <Widget>[
-	                  FadeAnimation(1.8, Container(
-	                    padding: EdgeInsets.all(5),
-	                    decoration: BoxDecoration(
-	                      color: Colors.white,
-	                      borderRadius: BorderRadius.circular(10),
-	                      boxShadow: [
-	                        BoxShadow(
-	                          color: Color.fromRGBO(143, 148, 251, .2),
-	                          blurRadius: 20.0,
-	                          offset: Offset(0, 10)
-	                        )
-	                      ]
-	                    ),
-	                    child: Column(
-	                      children: <Widget>[
-	                        Container(
-	                          padding: EdgeInsets.all(8.0),
-	                          decoration: BoxDecoration(
-	                            border: Border(bottom: BorderSide(color: Colors.grey[100]))
-	                          ),
-	                          child: TextField(
-	                            decoration: InputDecoration(
-	                              border: InputBorder.none,
-	                              hintText: "Email or Phone number",
-	                              hintStyle: TextStyle(color: Colors.grey[400])
-	                            ),
-	                          ),
-	                        ),
-	                        Container(
-	                          padding: EdgeInsets.all(8.0),
-	                          child: TextField(
-	                            decoration: InputDecoration(
-	                              border: InputBorder.none,
-	                              hintText: "Password",
-	                              hintStyle: TextStyle(color: Colors.grey[400])
-	                            ),
-															obscureText: true,
-	                          ),
-	                        )
-	                      ],
-	                    ),
-	                  )),
-	                  SizedBox(height: 30,),
-	                  FadeAnimation(2, Container(
-	                    height: 50,
-	                    decoration: BoxDecoration(
-	                      borderRadius: BorderRadius.circular(10),
-	                      gradient: LinearGradient(
-	                        colors: [
-														Colors.grey, Colors.blueGrey, Colors.grey
-	                        ]
-	                      )
-	                    ),
-	                    child: Center(
-	                      child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-	                    ),
-	                  )),
-	                  SizedBox(height: 110,),
-	                  FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Colors.white70),)),
-	                ],
-	              ),
-	            )
-	          ],
-	        ),
-	      ),
-      )
-    );
+        ));
   }
 }
